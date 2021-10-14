5aef398a9fcc508cfebca46d98c907cd7b7c3393
A.Yakubouski <a.yakubouski@naveksoft.com>
2021-10-14 18:17:41 +0300

    updaet unittest

--- push1st-py/api.test.py
+++ push1st-py/api.test.py
@@ -4,23 +4,22 @@ import push1st
 Host = "localhost"
 AppId = "app-test"
 
+CGREY = '\x1B[90m'
+CRED = '\x1B[91m'
+CEND = '\x1B[0m'
+
 class Test_api(unittest.TestCase):
     api = push1st.capi(AppId,Host)
     def test_GetAccessToken(self):
         res, token = self.api.GetAccessToken()
-        self.assertEqual(res,200)
-        self.assertTrue(len(token))
+        self.assertTrue(res == 200 and len(token) != 0)
+        print('{:>10}::{:.<30} pass ({})'.format('API','GetAccessToken',res))
 
     def test_GetSessionToken(self):
         res, token = self.api.GetSessionToken("session-id","channel-id")
-        self.assertEqual(res,200)
-        self.assertTrue(len(token))
+        self.assertTrue(res == 200 and len(token) != 0)
+        print('{:>10}::{:.<30} pass ({})'.format('API','GetSessionToken',res))
 
-    def test_GetAccessTokenLoad(self):
-        for x in range(1, 100):
-            res, token = self.api.GetAccessToken()
-            self.assertEqual(res,200)
-            self.assertTrue(len(token))
         
 if __name__ == '__main__':
     unittest.main()
