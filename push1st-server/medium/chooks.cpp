56f54a1db894fe37214a2899dc1facafbeded354
A.Yakubouski <a.yakubouski@naveksoft.com>
2021-11-04 09:53:43 +0300

    async hook processing

--- push1st-server/medium/chooks.cpp
+++ push1st-server/medium/chooks.cpp
@@ -1,20 +1,27 @@
 #include "chooks.h"
 #include "../core/csyslog.h"
+#include "../core/ci/casyncqueue.h"
+
+static core::casyncqueue HookProcessing{ 2, "hookmgm" };
 
 void cwebhook::Trigger(hook_t::type trigger, sid_t app, sid_t channel, sid_t session, json::value_t&& msg)
 { 
-	syslog.print(7, "[ WEBHOOK:%s ] %s %s#%s\n", std::string{ webEndpoint.hostport() }.c_str(), str(trigger), app.c_str(), channel.c_str());
-	Write("POST", webEndpoint.url(), {
-			{"Content-Type","application/json"},
-			{"Connection", !fdKeepAlive ? "close" : "keep-alive"},
-			{"Keep-Alive","30"}}, 
-			json::serialize({
-				{"event",str(trigger)},
-				{"app",app},
-				{"channel",channel},
-				{"session",session},
-				{"data", json::serialize(std::move(msg))},
+	HookProcessing.enqueue([](std::weak_ptr<cwebhook> obj, hook_t::type trigger, std::string app, std::string channel, std::string session, json::value_t msg) {
+		if (auto&& self{ obj.lock() }; self) {
+			syslog.print(7, "[ WEBHOOK:%s ] %s %s#%s\n", std::string{ self->webEndpoint.hostport() }.c_str(), str(trigger), app.c_str(), channel.c_str());
+			self->Write("POST", self->webEndpoint.url(), {
+				{"Content-Type","application/json"},
+				{"Connection", !self->fdKeepAlive ? "close" : "keep-alive"},
+				{"Keep-Alive","30"} },
+				json::serialize({
+					{"event",str(trigger)},
+					{"app",app},
+					{"channel",channel},
+					{"session",session},
+					{"data", json::serialize(std::move(msg))},
 			}));
+		}
+	}, weak_from_this(), trigger, app, channel, session, msg);
 }
 
 
@@ -54,14 +61,14 @@ inline bool cwebhook::Connect() {
 
 	if (sockaddr_storage sa; (res = inet::GetSockAddr(sa, webEndpoint.hostport(), fdSsl ? "443" : "80", AF_INET)) == 0 ) {
 		if (!fdSslCtx) {
-			if ((res = inet::TcpConnect(fdEndpoint, sa, false, 2500)) == 0) {
+			if ((res = inet::TcpConnect(fdEndpoint, sa, false, 800)) == 0) {
 				//inet::SetTcpCork(fdEndpoint, false);
 				inet::SetTcpNoDelay(fdEndpoint, true);
 				::shutdown(fdEndpoint, SHUT_RD);
 				return true;
 			}
 		}
-		else if ((res = inet::SslConnect(fdEndpoint, sa, false, 2500,fdSslCtx,fdSsl)) == 0) {
+		else if ((res = inet::SslConnect(fdEndpoint, sa, false, 800,fdSslCtx,fdSsl)) == 0) {
 			::shutdown(fdEndpoint, SHUT_RD);
 			return true;
 		}
@@ -73,6 +80,11 @@ inline bool cwebhook::Connect() {
 
 void cluahook::Trigger(hook_t::type trigger, sid_t app, sid_t channel, sid_t session, json::value_t&& msg) {
 	if (luaAllowed) {
-		jitLua.luaExecute(luaScript, "OnTrigger", { str(trigger), app, channel, session, json::serialize(std::move(msg)) });
+		HookProcessing.enqueue([](std::weak_ptr<cluahook> obj, hook_t::type trigger, std::string app, std::string channel, std::string session, json::value_t msg) {
+			if (auto&& self{ obj.lock() }; self) {
+				syslog.print(7, "[ LUAHOOK:%s ] %s %s#%s\n", self->luaScript.filename().c_str(), str(trigger), app.c_str(), channel.c_str());
+				self->jitLua.luaExecute(self->luaScript, "OnTrigger", { str(trigger), app, channel, session, json::serialize(std::move(msg)) });
+			}
+		}, weak_from_this(), trigger, app, channel, session, msg);
 	}
 }
--- push1st-server/medium/chooks.h
+++ push1st-server/medium/chooks.h
@@ -7,28 +7,13 @@
 #include "../core/ci/cjson.h"
 #include "../core/lua/clua.h"
 
-/*
-class cconnection : public inet::chttpconnection {
-public:
-	cconnection(const dsn_t& endpoint);
-	void Write(const std::string_view& method, const std::string_view& uri, std::unordered_map<std::string_view, std::string>&& headers = {}, std::string&& request = "");
-private:
-	inline bool Reconnect();
-	fd_t fdEndpoint{ -1 };
-	std::string fdHostPort;
-	inet::ssl_ctx_t fdSslCtx;
-	inet::ssl_t fdSsl;
-	core::cspinlock fdLock;
-};
-*/
-
 class chook {
 public:
 	virtual ~chook() { ; }
 	virtual void Trigger(hook_t::type trigger, sid_t app, sid_t channel, sid_t session, json::value_t&&) = 0;
 };
 
-class cwebhook : public chook, public inet::chttpconnection {
+class cwebhook : public chook, public inet::chttpconnection, public std::enable_shared_from_this<cwebhook> {
 public:
 	cwebhook(const std::string& endpoint, bool keepalive = false);
 	virtual ~cwebhook() { ; }
@@ -45,7 +30,7 @@ private:
 	core::cspinlock fdLock;
 };
 
-class cluahook : public chook {
+class cluahook : public chook, public std::enable_shared_from_this<cluahook> {
 public:
 	cluahook(const std::string& endpoint) : luaAllowed{ std::filesystem::exists(endpoint) }, luaScript{ endpoint } {; }
 	virtual ~cluahook() { ; }
